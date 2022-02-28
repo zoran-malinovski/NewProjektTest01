b5692b9f843b1d72f8bd14ba28d954e9c5b6dd92
Zoran <z.malinovski79@gmail.com>
2022-02-28 19:10:38 +0100

    Added Public static void with new data and made run test

--- src/com/company/UnderClass01.java
+++ src/com/company/UnderClass01.java
@@ -1,4 +1,15 @@
 package com.company;
 
-public class UnderClass01 extends NewClass01{
+public class UnderClass01 extends MobilPhone{
+
+    public static void main(String[] args) {
+        MobilPhone phone = new MobilPhone();
+        phone.brand = "Samsung";
+        phone.model = "13 21";
+        System.out.println("Mobil shop nr.001");
+        phone.onStock();
+        System.out.println("---------");
+        phone.showAttribute();
+    }
+
 }
